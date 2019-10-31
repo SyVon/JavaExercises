dbb764818a32e13f34d82136970f1699512c2641
svon <syrusyeung@gmail.com>
2019-10-31 17:39:00 -0600

    added animal package for inheritance practice

---
+++ PracticeClasses/src/com/animal/Fish.java
@@ -0,0 +1,42 @@
+package com.animal;
+
+public class Fish extends Animal{
+
+	private int gills;
+	private int eyes;
+	private int fins;
+	
+	
+	
+	public Fish(String name, int brain, int body, int size, int weight, int gills, int eyes, int fins) {
+		super(name, brain, body, size, weight);
+		
+		this.gills = gills;
+		this.eyes = eyes;
+		this.fins = fins;
+	}
+	
+	public void rest() {
+		
+	}
+	
+	public void swim() {
+		
+	}
+	
+	private void moveMuscles() {
+		
+	}
+	
+	private void moveBackFin() {
+		
+	}
+	
+	private void swim(int speed) {
+		moveMuscles();
+		moveBackFin();
+		super.move(speed);
+	}
+	
+	
+}
