d7556d689f8e0595be2b94f20663f29238f7afe8
svon <syrusyeung@gmail.com>
2019-10-31 14:34:39 -0600

    added com.syrus package with class Car

---
+++ PracticeClasses/src/com/syrus/Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.syrus;
+
+/**
+ * @author svon
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Car porsche = new Car();
+		porsche.setEngine("v6");
+		System.out.println(porsche.getEngine());
+		String test = porsche.getEngine();
+		System.out.println(test);
+		test = "talsjegh";
+		System.out.println(porsche.getEngine()); 
+
+	}
+
+}
