ce9351c033bc522e8d412a42622bbd50a657a982
svon <syrusyeung@gmail.com>
2019-11-30 14:38:12 -0700

    added polymorphism classes and new data structures project

--- PracticeClasses/src/com/polymorphism/Main.java
+++ PracticeClasses/src/com/polymorphism/Main.java
@@ -1,5 +1,119 @@
 package com.polymorphism;
 
-public class Main {
+import java.util.Objects;
 
+class Car {
+	private String name;
+	private boolean engine;
+	private int cylinders;
+	private int wheels;
+	
+	public Car(String name, int cylinders){
+		this.cylinders = cylinders < 0 ? 1: cylinders;
+		this.name = Objects.requireNonNull(name, "Unknown");
+		this.wheels = 4;
+		this.engine = true; 
+	}
+
+	public void startEngine() {
+		System.out.println("You started your " + this.engine + " engine");
+	}
+	
+	public void accelerate() {
+		System.out.println("Car -> Accelerate");
+	}
+	
+	public void brake() {
+		System.out.println("Car -> Brake");
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public boolean getEngine() {
+		return engine;
+	}
+
+	public int getCylinders() {
+		return cylinders;
+	}
+
+	public int getWheels() {
+		return wheels;
+	}
+	
+}
+
+class Corvette extends Car{
+
+
+	public Corvette() {
+		super("Corvette",6);
+	}
+	
+	@Override
+	public void accelerate() {
+		System.out.println("Corvette -> Accelerate");
+	}
+	
+	@Override
+	public void brake() {
+		System.out.println("Corvetter -> brake");
+	}	
+	
+}
+
+
+class Kia extends Car{
+
+
+	public Kia() {
+		super("Kia",6);
+	}
+	
+	@Override
+	public void accelerate() {
+		System.out.println("Kia-> Accelerate");
+	}
+	
+	@Override
+	public void brake() {
+		System.out.println("Kia -> brake");
+	}	
+	
+}
+
+
+class Subaru extends Car{
+
+
+	public Subaru() {
+		super("Subaru",6);
+	}
+	
+	@Override
+	public void accelerate() {
+		System.out.println("Subaru -> Accelerate");
+	}
+	
+	@Override
+	public void brake() {
+		System.out.println("Subaru -> brake");
+	}	
+	
+}
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Car car = new Car("base car", 8);
+		System.out.println(car.getName());
+		car.accelerate();
+		car.brake();
+		
+		Subaru subaru = new Subaru(); 
+		subaru.accelerate();
+	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JavaExercises
+
+Each structure is based on a fundamental of Java, and each package within is a conecpt of each. 
